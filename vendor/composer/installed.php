e884ab7074795ff7911ff9612bf7970f7b57e667
ZhangNing <zhangning0222@qq.com>
2021-06-01 16:34:44 +0800

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'pdd/sdk',
+  ),
+  'versions' => 
+  array (
+    'pdd/sdk' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
